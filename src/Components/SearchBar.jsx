1a0e69ed0dd4a0de06908500969db48d250d372f
Dye <anthony.dye@ies-co.com>
2022-05-16 16:07:57 -0500

    Added Function to the Search Bar Component

---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+const SearchBar = (props) => {
+    
+    const [serach, setSearch] = useState("");
+    function SearchResults(event) {
+        event.preventDefault();
+
+        let response = props.games.filter((element) => {
+            if (element.name.includes(search)) {
+             return true;   
+            }
+            else if (element.platform.includes(search)) {
+                return true
+            }
+            else if (element.genre.includes(search)) {
+                return true
+            }
+            else if (element.publisher.includes(search)) {
+                return true
+            }
+        });
+        props.setGames(respose);
+        setSearch("")
+        if (search === "") {
+            props.getGames()
+
+        }
+    }
+
+    return (
+        <div>
+            <form onSubmit={SearchResults}>
+                <div>
+                   <input
+                     type="text"
+                     placeholder="Search here..."
+                     value={search}
+                     onChange={(e) => setSearch(e.target.value)}
+                     /> 
+                     <button className="buttonsearch" type="submit">Search</button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default SearchBar;
